66bdd0c7e0b46ba3424f4019cf43300f2e277f1e
DistinctDynamo <nguyensteven578@gmail.com>
2026-02-22 12:42:38 -0500

    password encryption added

--- graphql/queries/userQueries.js
+++ graphql/queries/userQueries.js
@@ -6,8 +6,21 @@ import UserType from '../types/userTypes.js'
 const userQueries = {
     login:{
        type: UserType,
-       args: { password: { type: new GraphQLNonNull(GraphQLString) } },
-       resolve: async (_, { password }) => await User.findOne( {password} )
+       args: { email:{type: new GraphQLNonNull(GraphQLString)}, password: { type: new GraphQLNonNull(GraphQLString) } },
+       resolve: async (_, { email, password }) => {
+            const user = await User.findOne( {email} )
+            const isMatch = await user.isValidPassword(password);
+
+            if (!user) {
+            throw new Error('Invalid credentials');
+            }
+            if (!isMatch) {
+            throw new Error('Invalid password');
+            }
+
+            return user
+       } 
+          
     }
 }
 
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UsersSchema = new mongoose.Schema({
     username:{
@@ -27,30 +28,30 @@ const UsersSchema = new mongoose.Schema({
     }
 })
 
-UsersSchema.pre('save', function(next) {
-    this.updated_at = Date.now();
-});
-
-//userSchema.pre('save', async function(next) {
-//  try {
+//UsersSchema.pre('save', function(next) {
 //    this.updated_at = Date.now();
-//
-//   if (!this.isModified('password')) return;
-//    
-//    const salt = await bcrypt.genSalt(10);
-//    this.password = await bcrypt.hash(this.password, salt);
-//     
-//  } catch (error) {
-//    next(error); 
-//  }
 //});
 
-//userSchema.methods.isValidPassword = async function(password) {
-//  try {
-//    return await bcrypt.compare(password, this.password);
-//  } catch (error) {
-//    throw new Error('Password comparison failed');
-//  }
-//};
+UsersSchema.pre('save', async function() {
+  try {
+    this.updated_at = Date.now();
+
+   if (!this.isModified('password')) return;
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+     
+  } catch (error) {
+     
+  }
+});
+
+UsersSchema.methods.isValidPassword = async function(password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error('Password comparison failed');
+  }
+};
 
 module.exports = mongoose.model('Users', UsersSchema);
